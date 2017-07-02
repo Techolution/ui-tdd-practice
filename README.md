832f9a99843a5685baf72bd6b8fbc77466e57740
originalusrnm <jshornbuckle@gmail.com>
2017-07-02 17:20:22 -0400

    Update spec buesiness requirements

--- README.md
+++ README.md
@@ -12,9 +12,9 @@ npm
 3. To run tests type: `npm run test` each time you want to rerun the tests.
 
 ### The Holy Process of TDD
-1. Write a failing test first - R.ED
-2. Write the source code until the test passes - GREEN.
-3. Refactor the code ( ensuring the test remains GREEN ).
+1. Write a failing test first - RED
+2. Write the source code until the test passes - GREEN
+3. Refactor the code to make it better and more efficient ( ensuring the test remains GREEN )
 
 Fail, Pass, Refactor!
 
--- spec/BalancedParentheses.spec.js
+++ spec/BalancedParentheses.spec.js
@@ -1,13 +1,24 @@
-describe('test', function() {
+/*
+   Write a program to determine if the the parentheses (),
+   the brackets [], and the braces {}, in a string are balanced.
+
+    For example:
+
+    {{)(}} is not balanced because ) comes before (
+
+    ({)} is not balanced because ) is not balanced between {}
+    and similarly the { is not balanced between ()
+
+    [({})] is balanced
+
+    {}([]) is balanced
+
+    {()}[[{}]] is balanced
+*/
+
+describe('BalancedParentheses', function() {
 
 	var BalancedParentheses = require('../src/BalancedParentheses.js');
 
-	beforeEach(function() {
-	    balancedParentheses = new BalancedParentheses();
-  	});
 
-	it('should work', function() {
-		expect(balancedParentheses).toBeDefined();
-	});
-	
 });
